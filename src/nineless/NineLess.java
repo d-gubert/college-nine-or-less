1ef727b41ae58f35701fe8b5ba879924b2da847c
Douglas Gubert <douglas_gubert@hotmail.com>
2014-03-04 22:40:43 -0300

    Initial commit, barely project structure and the very beginning of the first solution

---
+++ src/nineless/NineLess.java
@@ -0,0 +1,21 @@
+
+package nineless;
+
+/**
+ *
+ * @author Douglas Gubert <douglas.gubert@gmail.com>
+ */
+public class NineLess {
+
+	public static void main(String[] args) {
+
+	}
+
+	public static void firstSolution () {
+		for (long seed = 100000000000L; seed < 999999999999L; seed++) {
+			String num = String.valueOf(seed);
+
+		}
+	}
+
+}
