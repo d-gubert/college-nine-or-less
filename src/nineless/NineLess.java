ac051ee46cbecf9bf40c0b79669c14fd82794298
Douglas Gubert <douglas_gubert@hotmail.com>
2014-03-04 23:57:37 -0300

    This alogorithm finds the last number that meets our criteria within the specified range (12-digit numbers)

--- src/nineless/NineLess.java
+++ src/nineless/NineLess.java
@@ -8,14 +8,29 @@ package nineless;
 public class NineLess {
 
 	public static void main(String[] args) {
-
+		firstSolution();
 	}
 
 	public static void firstSolution () {
-		for (long seed = 100000000000L; seed < 999999999999L; seed++) {
+		long answer = 0;
+		for (long seed = 999999999999L; seed >= 100000000000L; seed--) {
+			if ((seed % 999999999) == 0) System.out.println(seed+" "+answer);
+			boolean valid = true;
 			String num = String.valueOf(seed);
-
+			for (int i = 0; i < 10; i++) {
+				String[] nums = num.substring(i, i+3).split("");
+				if (Integer.valueOf(nums[1]) + Integer.valueOf(nums[2]) + Integer.valueOf(nums[3]) > 9) {
+//					System.out.println("FALSE!!!!:::" + seed);
+					valid = false;
+					break;
+				}
+			}
+			if (valid) {
+				System.out.println(num);
+				answer++;
+			}
 		}
+		System.out.println(answer);
 	}
 
 }
